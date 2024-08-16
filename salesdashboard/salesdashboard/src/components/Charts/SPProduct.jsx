2bea3017f9c0ec15687e933c9dc205da9f981c09
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-08-16 16:24:07 +0200

    charts.2

--- salesdashboard/salesdashboard/src/components/Charts/SPProduct.jsx
+++ salesdashboard/salesdashboard/src/components/Charts/SPProduct.jsx
@@ -11,22 +11,27 @@ import data from "../../Data/SalesPerProduct.json";
 
 function SPProduct() {
   return (
-    <BarChart
-      width={600}
-      height={300}
-      data={data}
-      margin={{ top: 20, right: 0, left: 0, bottom: 0 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="day" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="ProductA" stackId="a" fill="#06c" />
-      <Bar dataKey="ProductB" stackId="a" fill="#4cb140" />
-      <Bar dataKey="ProductC" stackId="a" fill="#009596" />
-      <Bar dataKey="ProductD" stackId="a" fill="#5752d1" />
-    </BarChart>
+    <div className="chart">
+      <div className="chart-heading">
+        <h2>Sales Per Product</h2>
+      </div>
+      <BarChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{ top: 20, right: 0, left: 0, bottom: 0 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="ProductA" stackId="a" fill="#06c" />
+        <Bar dataKey="ProductB" stackId="a" fill="#4cb140" />
+        <Bar dataKey="ProductC" stackId="a" fill="#009596" />
+        <Bar dataKey="ProductD" stackId="a" fill="#5752d1" />
+      </BarChart>
+    </div>
   );
 }
 
--- salesdashboard/salesdashboard/src/components/Charts/SSales.jsx
+++ salesdashboard/salesdashboard/src/components/Charts/SSales.jsx
@@ -11,21 +11,26 @@ import data from "../../Data/Sales.json";
 
 function SSales() {
   return (
-    <LineChart
-      width={600}
-      height={300}
-      data={data} // access the array of objects within the data object
-      margin={{ top: 20, right: 0, left: 0, bottom: 0 }}
-      padding={0}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="day" />
-      <YAxis dataKey="TotalSales" />
-      <Tooltip />
-      <Legend />
+    <div className="chart">
+      <div className="chart-heading">
+        <h2>Sales</h2>
+      </div>
+      <LineChart
+        width={600}
+        height={300}
+        data={data} // access the array of objects within the data object
+        margin={{ top: 20, right: 0, left: 0, bottom: 0 }}
+        padding={0}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day" />
+        <YAxis dataKey="TotalSales" />
+        <Tooltip />
+        <Legend />
 
-      <Line type="monotone" dataKey="TotalSales" stroke="#750505" />
-    </LineChart>
+        <Line type="monotone" dataKey="TotalSales" stroke="#06c" />
+      </LineChart>
+    </div>
   );
 }
 
