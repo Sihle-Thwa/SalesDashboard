101ef506ead40f5ca6f66f5093a1cc0ec8d8ae7b
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-08-19 16:49:20 +0200

    Updated Sidenavigation

--- salesdashboard/salesdashboard/src/components/Navigation/SideNav.jsx
+++ salesdashboard/salesdashboard/src/components/Navigation/SideNav.jsx
@@ -1,93 +1,91 @@
-import { BsColumns, BsFileEarmarkText, BsGear, BsGraphUp, BsPc, BsPeople, BsQuestion, BsSpeedometer, BsTicket } from "react-icons/bs";
+import {
+  BsColumns,
+  BsFileEarmarkText,
+  BsGear,
+  BsGraphUp,
+  BsPc,
+  BsPeople,
+  BsQuestion,
+  BsSpeedometer,
+  BsTicket,
+} from "react-icons/bs";
 
 function SideNav() {
+
+  const navItems = [
+    {
+      href: "/dashboard",
+      icon: <BsColumns />,
+      label: "Dashboard",
+    },
+    {
+      href: "/report",
+      icon: <BsFileEarmarkText />,
+      label: "Report",
+    },
+    {
+      href: "/product",
+      icon: <BsTicket />,
+      label: "Product",
+    },
+    {
+      href: "#",
+      icon: <BsGraphUp />,
+      label: "Analytics",
+    },
+    {
+      href: "#",
+      icon: <BsSpeedometer />,
+      label: "Performance",
+    },
+    {
+      href: "/customers",
+      icon: <BsPeople />,
+      label: "Customers",
+    },
+    {
+      href: "#",
+      icon: <BsQuestion />,
+      label: "Help center",
+    },
+    {
+      href: "#",
+      icon: <BsGear />,
+      label: "Settings",
+    },
+    {
+      href: "#",
+      icon: <BsPc />,
+      label: "Logout",
+    },
+  ];
+  
   return (
-    <nav className="nav d-flex flex-column flex-shrink-0 p-1 text-white bg-dark w-100 md-w-75" style={{ height: "100vh" }}>
+    <nav
+      className="nav d-flex flex-column flex-shrink-0 p-1 text-black bg-light w-75"
+      style={{ height: "100vh" }}
+    >
       <a
         href="/home"
-        className="d-flex align-items-center mb-2 mb-md-0 me-md-auto text-white text-decoration-none"
+        className="d-flex align-items-center mb-2 mb-md-0 me-md-auto text-black text-decoration-none"
       >
-        <h1>Sales4U</h1>
+        <h1 className="d-none d-lg-block">Sales4U</h1>
       </a>
-      <hr />
+      <hr className="d-none d-lg-block" />
 
       <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/dashboard">
-            <BsColumns />
-            <span className="ms-1 d-sm-inline d-none">Dashboard</span>
-
-          </a>
-        </li>
+        {navItems.map((item) => (
+          <li key={item.href} className="nav-item">
+            <a className="nav-link" href={item.href}>
+              {item.icon}
+              <span className="ms-1 d-sm-inline d-none">{item.label}</span>
+            </a>
+          </li>
+        ))}
       </ul>
-
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link align-middle -sm-0" href="/report">
-            <BsFileEarmarkText />
-            <span className="ms-1 d-sm-inline d-none">Report</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="/product">
-            <BsTicket />
-            <span className="ms-1 d-sm-inline d-none">Product</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            <BsGraphUp />
-            <span className="ms-1 d-sm-inline d-none">Analytics</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            <BsSpeedometer />
-            <span className="ms-1 d-sm-inline d-none">Performance</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a className="nav-link" href="/customers">
-            <BsPeople />
-            <span className="ms-1 d-sm-inline d-none">Customers</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            <BsQuestion />
-            <span className="ms-1 d-sm-inline d-none">Help center</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            <BsGear />
-            <span className="ms-1 d-sm-inline d-none">Settings</span>
-          </a>
-        </li>
-      </ul>
-      <ul className="nav nav-pills flex-column mb-1">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            <BsPc />
-            <span className="ms-1 d-sm-inline d-none">Logout</span>
-          </a>
-        </li>
-      </ul>
-
     </nav>
   );
 }
+
 
 export default SideNav;
